18526aabbac19186681d56480036692266c971bc
matthiasrondenet <matthiasrondenet@gmail.com>
2025-11-13 15:56:23 +0100

    review app routes lazy load

--- src/app-routes.tsx
+++ src/app-routes.tsx
@@ -1,32 +1,41 @@
-import React from "react";
 import type { RouteRecord } from "vite-react-ssg";
-
-const MainLayout = React.lazy(() => import("./components/layout/main-layout"));
-const Home = React.lazy(() => import("@/components/pages/home/home"));
-const Playground = React.lazy(
-  () => import("@/components/pages/playground/playground")
-);
+import { presetKeys } from "@/services/presets/presets";
 
 export const routes: RouteRecord[] = [
   {
     path: "/",
-    element: <MainLayout />,
-    entry: "src/main-layout.tsx",
+    lazy: async () => {
+      const module = await import("./components/layout/main-layout");
+      return { Component: module.default };
+    },
+    entry: "src/components/layout/main-layout.tsx",
     children: [
       {
         index: true,
-        Component: Home,
+        lazy: async () => {
+          const module = await import("@/components/pages/home/home");
+          return { Component: module.default };
+        },
         entry: "src/components/pages/home/home.tsx",
       },
       {
         path: "playground",
-        Component: Playground,
+        lazy: async () => {
+          const module = await import("@/components/pages/playground/playground");
+          return { Component: module.default };
+        },
         entry: "src/components/pages/playground/playground.tsx",
       },
       {
         path: "presets/:preset?",
-        Component: Playground,
+        lazy: async () => {
+          const module = await import("@/components/pages/playground/playground");
+          return { Component: module.default };
+        },
         entry: "src/components/pages/playground/playground.tsx",
+        getStaticPaths: () => {
+          return Object.values(presetKeys).map((key) => `presets/${key}`);
+        },
       },
       {
         path: "*",
